e81839a72b275a02bd7a1b5c20b3208a32c47066
mario <ochoam444@gmail.com>
2022-10-21 12:36:55 +0200

    Primer commit

---
+++ 1-Ramp Up/1-Python/1-Basics/ejemplo1.py
@@ -0,0 +1,8 @@
+var1 = 200
+var2 = 300
+
+var1
+
+print(var1 + var2)
+
+var2
---
+++ 2-Data Analysis/2-Pandas/Teoria/pd_example.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df1 = pd.DataFrame({"A": [1, 2, 3], "B":[4, 5, 6]})
+df2 = pd.DataFrame({"A": [7, 8, 9], "B":[10, 11, 12]})
+
+print(pd.concat([df1, df2]))
